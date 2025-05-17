8f84570fc76468072269304cdae029d75e61d620
Jason Chang <changjason96@gmail.com>
2025-05-16 22:11:06 -0400

    added contants for hard coded data

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
 const net=require("net");
+const {IP, PORT} = require("./constants");
 const connect = function() {
    const conn = net.createConnection({
-    host:"localhost",
-    port: 50541, // num
+    host:IP,
+    port:PORT, // num
    });
    conn.setEncoding("utf8");
 
