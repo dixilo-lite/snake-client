5c64fa7735d5f20f905b62c501201e6b6f16d4da
Jason Chang <changjason96@gmail.com>
2025-05-16 22:11:44 -0400

    refactored: removed comments

--- client.js
+++ client.js
@@ -12,14 +12,6 @@ const connect = function() {
     conn.write("Name: J.C");
     conn.write("Move: left");
     conn.write("Move: up");
-    /*setTimeout(() => {
-      conn.write("Move: up");
-    },50);
-    setTimeout(() => {
-      conn.write("Move: left");
-    },100);
-    
-     setInterval(()=> {conn.write("Move: up")},1000);*/
    });
 
    //returns data from the server
