9312e76a146b27e0be8883ba7284182f7c777f2c
Jason Chang <changjason96@gmail.com>
2025-05-16 21:10:49 -0400

    added movement with setInterval and setTimeout

--- client.js
+++ client.js
@@ -9,6 +9,16 @@ const connect = function() {
    conn.on("connect",() => {
     console.log("Connected!");
     conn.write("Name: J.C");
+    conn.write("Move: left");
+    conn.write("Move: up");
+    /*setTimeout(() => {
+      conn.write("Move: up");
+    },50);
+    setTimeout(() => {
+      conn.write("Move: left");
+    },100);
+    
+     setInterval(()=> {conn.write("Move: up")},1000);*/
    });
 
    //returns data from the server
