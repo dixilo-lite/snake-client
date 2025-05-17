c356d1accf832c9aecad165b8a9cf799b26c4fad
Jason Chang <changjason96@gmail.com>
2025-05-16 22:07:40 -0400

    added messages

--- input.js
+++ input.js
@@ -11,25 +11,33 @@ const setupInput = function(conn) {
   return stdin;
 };
 
-const handleUserInput =function (key){
+const handleUserInput =function (key) {
   if (key ==="\u0003"){
     process.exit();
-}
-else if (key ==="w"){
-  connection.write("Move: up");
-}
-else if (key === "a")
-{
-  connection.write("Move: left");
-}
-else if (key === "s")
-{
-  connection.write("Move: down");
-}
-else if (key === "d")
-{
-  connection.write("Move: right")
-}
+  }
+  else if (key ==="w"){
+    connection.write("Move: up"); 
+  }
+  else if (key === "a")
+  {
+    connection.write("Move: left");
+  }
+  else if (key === "s")
+  {
+    connection.write("Move: down");
+  }
+  else if (key === "d")
+  {
+    connection.write("Move: right")
+  }
+  else if (key === "r")
+  {
+    connection.write("Say: Go big or go home!");
+  }
+  else if (key === "q")
+  {
+    connection.write ("Say: Come here!");
+  }
 }
 
 module.exports=setupInput;
